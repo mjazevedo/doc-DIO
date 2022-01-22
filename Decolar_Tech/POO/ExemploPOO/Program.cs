0592035312ed515a6a0d22547ab731632347f05f
mjazevedo <matheusjazevedo1@gmail.com>
2022-01-22 16:30:37 -0300

    exemplos POO

--- Decolar_Tech/POO/ExemploPOO/Program.cs
+++ Decolar_Tech/POO/ExemploPOO/Program.cs
@@ -7,15 +7,30 @@ namespace ExemploPOO
     {
         static void Main(string[] args)
         {
+            
+            Aluno a1 = new Aluno();
+            a1.Nome = "João Marques";
+            a1.Idade = 19;
+            a1.Nota = 10;
+
+            a1.Apresentar();
+
+            Professor p1 = new Professor();
+            p1.Nome = "Antônio Fernandes";
+            p1.Salario = 4000.00;
+            p1.Idade = 38;
+
+            p1.Apresentar();
+
             // Valores válidos
-            Retangulo r = new Retangulo();
-            r.DefinirMedidas(30,20);
-            System.Console.WriteLine($"Área = {r.ObterArea()}");
+            // Retangulo r = new Retangulo();
+            // r.DefinirMedidas(30,20);
+            // System.Console.WriteLine($"Área = {r.ObterArea()}");
 
             //Valores inválidos
-            Retangulo r2 = new Retangulo();
-            r2.DefinirMedidas(0,0);
-            System.Console.WriteLine($"Área = {r2.ObterArea()}");
+            // Retangulo r2 = new Retangulo();
+            // r2.DefinirMedidas(0,0);
+            // System.Console.WriteLine($"Área = {r2.ObterArea()}");
 
 
 
