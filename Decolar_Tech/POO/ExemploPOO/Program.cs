e2d51de772f3933598ad89ee2f5a2dba0316c4bd
mjazevedo <matheusjazevedo1@gmail.com>
2022-01-22 12:04:01 -0300

    exemplos

---
+++ Decolar_Tech/POO/ExemploPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Matheus";
+            p1.Idade = 21;
+
+            p1.Apresentar();
+        }
+    }
+}
